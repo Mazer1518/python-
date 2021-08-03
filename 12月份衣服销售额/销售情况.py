996c568544744c654f9193ccea852e9595bafb83
Mazer <2920140779@qq.com>
2021-08-03 11:02:10 +0800

    数据分析

---
+++ 12月份衣服销售额/销售情况.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_excel('./12月份衣服销售数据.xlsx')
+
+
+sum1 = 0
+share = ['羽绒服', '牛仔裤', '风衣', 'T血', '皮草', '衬衫']
+
+
+def s(groups1):
+    if groups1['库存数量'].iloc[0] < groups1['销售量/每日'].sum():
+        return groups1['库存量'] * groups1['价格/件'].iloc[0]
+    else:
+        return groups1['销售量/每日'].sum() * groups1['价格/件'].iloc[0]
+
+
+for i in share:
+    group = df.groupby('服装名称').get_group(i)
+    sum1 += s(group)
+
+print('总计：', sum1)
+print("服装名称   销售量占比")
+for i in share:
+    group = df.groupby('服装名称').get_group(i)
+    sum0 = s(group)
+    print(i, "%.2f" % (sum0 / sum1 * 100) + '%')
